05e166291bca21cb0bda3c314239d75c1e5ea589
aliicoder <k.c.o.c.3000@gmail.com>
2025-03-31 01:26:42 +0530

    debug 8

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react"
 import PrimaryButton from "../buttons/PrimaryButton"
 import Image from "next/image"
--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react"
 import { FaTwitter } from "react-icons/fa6"
 import { PiTwitchLogoFill } from "react-icons/pi"
