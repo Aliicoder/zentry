b268899fe77e1dc1ba5495fca7b7d6c19fe8c1b0
aliicoder <k.c.o.c.3000@gmail.com>
2025-03-31 01:10:22 +0530

    debug 5

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   //distDir: "build", // Use a relative path without "/"
-  eslint: {
-    ignoreDuringBuilds: true, // Ignores linting errors during build
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Ignores TypeScript errors during build
-  },
 }
 
 export default nextConfig
